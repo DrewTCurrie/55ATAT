140c88f0623cfb32e66b2fd5b329a82323dc5cc4
drewC <drewcurt6@gmail.com>
2024-10-06 14:00:33 -0600

    Changed font back and set to 50 size

--- APIFuncs/badgeGenerator.py
+++ APIFuncs/badgeGenerator.py
@@ -27,12 +27,12 @@ def generate_qr_code(userID, filename):
 
 
 # Function to add text to an image
-def add_text_to_image(img_path, text, output_path, position, font_size=100):
+def add_text_to_image(img_path, text, output_path, position, font_size=50):
     img = Image.open(img_path).convert("RGBA")
     draw = ImageDraw.Draw(img)
     font = ImageFont.load_default()
     try:
-        font = ImageFont.truetype("Quicksand-Bold.ttf", font_size)
+        font = ImageFont.truetype("Arial.ttf", font_size)
     except IOError:
         font = ImageFont.load_default()
 
