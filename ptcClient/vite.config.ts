ae8c49d6e06f27ada976484b45a1446c9157981e
Drew Currie <drew.currie@student.montana.edu>
2024-10-09 12:08:14 -0600

    Added port 80 as node port for browser connection

--- ptcClient/vite.config.ts
+++ ptcClient/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 80,
     proxy: {
       '/api': {
         target: 'http://192.168.26.7:5000',
