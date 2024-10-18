ba225775959fcac8fec94ddf0193193f0b9e22d3
drewC <drewcurt6@gmail.com>
2024-10-17 21:18:48 -0600

    Updated proxy

--- ptcClient/vite.config.ts
+++ ptcClient/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://192.168.26.7:5000',
         changeOrigin: true,
         secure: false,
       },
