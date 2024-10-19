f54112e354ab43d94f46058c38ead4fb03079637
drewC <drewcurt6@gmail.com>
2024-10-19 15:42:18 -0600

    Garbage Collector that removes the profilepictures, badges, and old reports

--- flaskServer/garbagecollector.py
+++ flaskServer/garbagecollector.py
@@ -3,12 +3,44 @@ import os, sys
 def RemoveOldReports():
     path = "./xlsx/"
     ReportsList = os.listdir(path)
-    print("Files located inside the " + path + "directory: ")
+    print("Removing old reports located in " + path + ":")
     print(ReportsList)
+    for report in ReportsList: 
+        if report == ".gitkeep":
+            print("Keeping " + report + "as it is not a report and is required for system operation.")
+        else:   
+            if os.path.isfile(path+report):
+                os.remove(path+report)
+        
+
 
 def RemoveOldBadges():
+    path ="./static/"
+    BadgeGenerationFileList = os.listdir(path)
+    print("Removing old badges located in " + path + ":")
+    print(BadgeGenerationFileList)
+    for BadgeArtifact in BadgeGenerationFileList:
+        if BadgeArtifact == "BadgeTemplates":
+            print("Ignoring badge templates")
+        elif BadgeArtifact == ".gitignore":
+            print("Keeping" + BadgeArtifact + "as it is not an artifact of creating a badege and is required for system operation.")
+        else:
+            if os.path.isfile(path+BadgeArtifact):
+                os.remove(path+BadgeArtifact)
+
     
+def RemoveOldProfilePictures():
+    path = "./profileImage/"
+    ProfilePictureList = os.listdir(path)
+    print("Removing old profile pictures located in " + path + ":")
+    print(ProfilePictureList)
+    for ProfilePicture in ProfilePictureList:
+        if ProfilePicture == ".gitkeep":
+            print("Keeping" + ProfilePicture + "as it is not an artifact of creating a badege and is required for system operation.")
+        else:
+            if os.path.isfile(path+ProfilePicture):
+                os.remove(path+ProfilePicture)
 
 
 if __name__ == '__main__':
-    sys.exit(RemoveOldReports())
+    sys.exit(RemoveOldProfilePictures())
