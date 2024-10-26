9055645131a164698edc1098f21ffc6d002c2f4b
drewC <drewcurt6@gmail.com>
2024-10-25 21:02:38 -0600

    Garbage collector with dynamic pathing

--- flaskServer/garbagecollector.py
+++ flaskServer/garbagecollector.py
@@ -1,7 +1,14 @@
+#Created 10/19/2024 Drew Currie
+#Last editted: 10/19/2024 DC
+#Garbage collector for left over files created during normal system operation
+#This will check for old reports, old badges, old profile pictures and remove them.
+#one function was written for each so that it would be modular. To call each in one function
+#The collectGarbage function calls each in order. 
 import os, sys
 
 def RemoveOldReports():
-    path = "./xlsx/"
+    currentWorkingDirectory = os.path.abspath(os.getcwd())
+    path = os.path.join(currentWorkingDirectory, 'flaskServer', 'xlsx/')
     ReportsList = os.listdir(path)
     print("Removing old reports located in " + path + ":")
     print(ReportsList)
@@ -15,7 +22,8 @@ def RemoveOldReports():
 
 
 def RemoveOldBadges():
-    path ="./static/"
+    currentWorkingDirectory = os.path.abspath(os.getcwd())
+    path = os.path.join(currentWorkingDirectory, 'flaskServer', 'static/')
     BadgeGenerationFileList = os.listdir(path)
     print("Removing old badges located in " + path + ":")
     print(BadgeGenerationFileList)
@@ -30,17 +38,25 @@ def RemoveOldBadges():
 
     
 def RemoveOldProfilePictures():
-    path = "./profileImage/"
+    currentWorkingDirectory = os.path.abspath(os.getcwd())
+    path = os.path.join(currentWorkingDirectory, 'flaskServer', 'profileImage/')
     ProfilePictureList = os.listdir(path)
     print("Removing old profile pictures located in " + path + ":")
     print(ProfilePictureList)
     for ProfilePicture in ProfilePictureList:
         if ProfilePicture == ".gitkeep":
-            print("Keeping" + ProfilePicture + "as it is not an artifact of creating a badege and is required for system operation.")
+            print("Keeping" + ProfilePicture + "as it is not an artifact of creating a badge and is required for system operation.")
         else:
             if os.path.isfile(path+ProfilePicture):
                 os.remove(path+ProfilePicture)
 
 
+def CollectGarbage():
+    RemoveOldReports()
+    RemoveOldBadges()
+    RemoveOldProfilePictures()
+
+
+
 if __name__ == '__main__':
-    sys.exit(RemoveOldProfilePictures())
+    sys.exit(CollectGarbage())
